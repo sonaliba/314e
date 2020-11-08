23c9acb9fee3b10737a8d5d64295e5dded2bec7f
unknown <batra.sonali@gmail.com>
2020-11-08 18:00:57 +0530

    Minor changes

--- src/Parser.java
+++ src/Parser.java
@@ -16,9 +16,9 @@ import org.jsoup.select.Elements;
 
 public class Parser {
 
-	String main_url = "https://www.314e.com"; //Change this url as per requirement
+	String main_url = "https://www.314e.com"; // Change this url as per requirement
 
-	HashSet<String> urls = new HashSet<String>(); //unique set of url's to be parsed
+	HashSet<String> urls = new HashSet<String>(); // unique set of url's to be parsed
 
 	HashMap<String, Integer> words_frequency = new HashMap<String, Integer>();
 
@@ -46,117 +46,114 @@ public class Parser {
 
 	}
 
-	public void sort_words_frequency() { //takes hash map of most frequent words and sorts it in descending order
-		
+	public void sort_words_frequency() { // takes hash map of most frequent words and sorts it in descending order
+
 		ArrayList<Integer> list = new ArrayList<>();
-		
+
 		for (Map.Entry<String, Integer> entry : words_frequency.entrySet()) {
-			
+
 			list.add(entry.getValue());
-			
+
 		}
-		
+
 		Collections.sort(list, Collections.reverseOrder());
 
 		for (int num : list) {
-			
+
 			for (Entry<String, Integer> entry : words_frequency.entrySet()) {
-				
+
 				if (entry.getValue().equals(num)) {
-					
+
 					sortedMap_words.put(entry.getKey(), num);
-					
+
 				}
 			}
 		}
 	}
 
-	public void sort_words_pairs_frequency() { //takes hash map of most frequent word pairs and sort in descending order
-		
+	public void sort_words_pairs_frequency() { // takes hash map of most frequent word pairs and sort in descending
+												// order
+
 		ArrayList<Integer> list = new ArrayList<>();
-		
+
 		for (Map.Entry<String, Integer> entry : word_pairs_frequency.entrySet()) {
-			
+
 			list.add(entry.getValue());
-			
+
 		}
 		Collections.sort(list, Collections.reverseOrder());
 
 		for (int num : list) {
-			
+
 			for (Entry<String, Integer> entry : word_pairs_frequency.entrySet()) {
-				
+
 				if (entry.getValue().equals(num)) {
-					
+
 					sortedMap_word_pairs.put(entry.getKey(), num);
-					
+
 				}
 			}
 		}
 	}
 
-	public void display_words_frequency() { //displays top 10 most frequest words
-		
+	public void display_words_frequency() { // displays top 10 most frequent words
+
 		int count = 0;
 
 		for (Map.Entry<String, Integer> entry : sortedMap_words.entrySet()) {
-			
+
 			System.out.println("word = " + entry.getKey() + ", frequency = " + entry.getValue());
-			
+
 			count++;
 
 			if (count == 10)
-				
+
 				break;
 		}
 	}
 
-	public void display_word_pairs_frequency() { //displays top 10 most frequest word pairs
-		
+	public void display_word_pairs_frequency() { // displays top 10 most frequest word pairs
+
 		int count = 0;
 
 		for (Map.Entry<String, Integer> entry : sortedMap_word_pairs.entrySet()) {
-			
+
 			System.out.println("word pair = " + entry.getKey() + ", frequency = " + entry.getValue());
-			
+
 			count++;
 
 			if (count == 10)
-				
+
 				break;
 		}
 
 	}
 
-	public void count_words_hashset() { 
+	public void count_words_hashset() {
 
 		for (String link : urls) {
-			
+
 			count_words(link);
+
 			count_word_pairs(link);
 
 		}
 	}
 
-	public void count_word_pairs(String link) { //parser web page to count frequency of word pairs by using a hash map
-		
-		Document doc= null;
-		
+	public void count_word_pairs(String link) { // parser web page to count frequency of word pairs by using a hash map
+
+		Document doc = null;
+
 		try {
-			
+
 			doc = Jsoup.connect(link).get();
-			
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			
-			System.out.println("Exception in count_word_pairs: This URL does not exist or is not valid for this problem");
-			
-			e.printStackTrace();
+
 		}
-		
 
 		if (doc != null) {
-			
+
 			String text = doc.body().text();
 
 			String[] sentences = text.split("\\.");
@@ -166,16 +163,17 @@ public class Parser {
 				String[] words = sentences[i].split("[\\s,;]+");
 
 				for (int j = 0; j < words.length - 1; j++) {
+
 					String word_pair = words[j] + "|" + words[j + 1];
 
 					if (!word_pairs_frequency.containsKey(word_pair)) {
-						
+
 						word_pairs_frequency.put(word_pair, 1);
 					}
 
 					else {
 						int val = word_pairs_frequency.get(word_pair);
-						
+
 						word_pairs_frequency.put(word_pair, ++val);
 					}
 
@@ -186,33 +184,28 @@ public class Parser {
 		}
 	}
 
-	
-	public void count_words(String link) { //parses web page to count frequency of words by using a hash map
-		
-		Document doc= null;
-		
+	public void count_words(String link) { // parses web page to count frequency of words by using a hash map
+
+		Document doc = null;
+
 		try {
-			
+
 			doc = Jsoup.connect(link).get();
-			
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			
-			System.out.println("Exception in count_words: This URL does not exist or is not valid for this problem");
-			
-			e.printStackTrace();
+
 		}
 
 		if (doc != null) {
-			
+
 			String text = doc.body().text();
 
 			String[] words = text.split("[\\s,;.]+");
 
 			for (int i = 0; i < words.length; i++) {
-				
+
 				if (!words_frequency.containsKey(words[i])) {
-					
+
 					words_frequency.put(words[i], 1);
 				}
 
@@ -220,7 +213,7 @@ public class Parser {
 
 				{
 					int previous_count = words_frequency.get(words[i]);
-					
+
 					words_frequency.put(words[i], ++previous_count);
 				}
 			}
@@ -229,8 +222,9 @@ public class Parser {
 
 	}
 
-	public void level_order_traversal() { // A breadth first traversal is done of the tree consisting of the hyperlinks upto level 4
-										 
+	public void level_order_traversal() { // A breadth first traversal is done of the tree consisting of the hyperlinks
+											// upto level 4
+
 		Queue<String> hyperLinks = new LinkedList<String>();
 
 		hyperLinks.add(main_url);
@@ -244,27 +238,32 @@ public class Parser {
 			int n = hyperLinks.size();
 
 			while (n > 0) {
-				
+
 				String url_retrieved = hyperLinks.remove();
 
 				Document document = null;
 				try {
+
 					document = Jsoup.connect(url_retrieved).get();
+
 				} catch (IOException e) {
-					System.out.println(
-							"Exception in level order traversal: This URL does not exist or is not valid for this problem");
-					e.printStackTrace();
+
 				}
 
 				if (document != null) {
+
 					Elements links = document.getElementsByTag("a");
 
 					for (Element link : links) {
 
 						if (link.attr("href").startsWith(main_url)) {
+
 							if (!urls.contains(link.attr("href"))) {
+
 								hyperLinks.add(link.attr("href"));
+
 								urls.add(link.attr("href"));
+
 							}
 						}
 					}
@@ -275,7 +274,9 @@ public class Parser {
 			}
 
 			count++;
+
 			System.out.println("URL's Upto level " + count);
+
 			System.out.println(urls);
 		}
 
